95fdfbcf64a9a666cd234f4bb4f5ee72d98e208a
Marek <mpietrewicz@gmail.com>
2019-01-13 14:55:35 +0100

    Ćwiczenie – byType, primary (02_IoC.pdf, str: 42)

--- src/main/java/pl/kurs/Operator.java
+++ src/main/java/pl/kurs/Operator.java
@@ -2,24 +2,30 @@ package pl.kurs;
 
 import lombok.Setter;
 
+import java.util.List;
+
 @Setter
 public class Operator {
-	CarShop carShop;
+	List<CarShop> carShops;
 
 	public void prepareData() {
-		for (int i = 0; i < 5; i++) {
-			Car car = new Car();
-			car.setMake("BMW");
-			car.setModel("seria " + (i+1));
-			car.setPrice(1000 + i);
-			carShop.addCar(car);
+		for (CarShop carShop : carShops) {
+			for (int i = 0; i < 5; i++) {
+				Car car = new Car();
+				car.setMake("BMW");
+				car.setModel("seria " + (i+1));
+				car.setPrice(1000 + i);
+				carShop.addCar(car);
+			}
 		}
 	}
 
 	public void printData() {
-		System.out.println("Salon: " + carShop.getName() + " wlasciciel: " + carShop.getOwner().getName());
-		for (Car car : carShop.getCars()) {
-			System.out.println(car.getMake() + " " + car.getModel() + " " + car.getPrice());
+		for (CarShop carShop : carShops) {
+			System.out.println("Salon: " + carShop.getName() + " wlasciciel: " + carShop.getOwner().getName());
+			for (Car car : carShop.getCars()) {
+				System.out.println(car.getMake() + " " + car.getModel() + " " + car.getPrice());
+			}
 		}
 	}
 
