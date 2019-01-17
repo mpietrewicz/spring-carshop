b4ecd32dbc3fa8ab475fa116207af913390fa370
Marek <mpietrewicz@gmail.com>
2019-01-17 23:38:25 +0100

    Ćwiczenie - ApplicationContextAware (02_IoC.pdf, str: 47)

--- src/main/java/pl/kurs/Start.java
+++ src/main/java/pl/kurs/Start.java
@@ -7,9 +7,8 @@ public class Start {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("kurs.xml");
-		Operator operator = context.getBean("operator", Operator.class);
-		operator.printData();
-		operator.getCarShops().get(0).getOwner().setName("Waldek");
+		Operator operator = context.getBean("operator2", Operator.class);
+		operator.prepareData();
 		operator.printData();
 	}
 
