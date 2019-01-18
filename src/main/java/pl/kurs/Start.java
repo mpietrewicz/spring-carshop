f38960144e27cda3ad139ab83f8ebca0b5021ae7
Marek <mpietrewicz@gmail.com>
2019-01-18 18:31:59 +0100

    Ćwiczenie – różne (03_IoC_2.pdf do str. 7)

--- src/main/java/pl/kurs/Start.java
+++ src/main/java/pl/kurs/Start.java
@@ -7,6 +7,13 @@ public class Start {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("kurs.xml");
+
+		System.out.println("   ---   ");
+		System.out.println("context.containsBean Test: " +context.containsBean("Test"));
+		System.out.println("context.getType: " +context.getType("car"));
+		System.out.println("context.isPrototype: " +context.isPrototype("car"));
+		System.out.println("   ---   ");
+
 		Operator operator = context.getBean("operator3", Operator.class);
 		operator.prepareData();
 		operator.printData();
